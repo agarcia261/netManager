8ff5cf937abcfdf869e62ae3614a6ecb251da8af
Angel <agarcia261@gmail.com>
2019-03-16 22:08:47 -0400

    arp table command working

--- controllers/ssh.js
+++ controllers/ssh.js
@@ -6,86 +6,67 @@ fs = require('fs')
 // ssh = new node_ssh()
 
 module.exports = {
-    adminDisplay : function (req, res){
+    adminDisplay : (req, res) =>{
  
-var conn = new Client();
-conn.on('ready', function() {
-    console.log('Client :: ready');
-    conn.shell(function(err, stream) {
-        let datatobSent=""
-        if (err) throw err;
-        stream.on('close', function() {
-            console.log('Stream :: close');
-            conn.end();
+        const conn = new Client();
+        conn.on('ready', () => {
+            console.log('Client :: ready');
+    
+            conn.shell((err, stream) =>{
+                let datatobSent=""
+                if (err) throw err;
 
-            const regex = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[^\r]*/g
-            let resu = datatobSent.match(regex)
-            let newArray=[]
-            let filteredArray=[]            
+                stream.on('close', () => {
+                    console.log('Stream :: close');
+                    conn.end();
 
-            // console.log(resu)
-            function splitArrays (array){
-                for (let i=0; i<array.length; i++){
+                    const regex = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[^\r]*/g
+                    let resu = datatobSent.match(regex)
+                    let result = [];            
 
-                    newArray.push(array[i].split(" "))
-
-                    for (let j=0; j<newArray[i].length; j++){
-                        console.log(newArray[i].filter(function (el) {
-                                     return el != null;
-                               }))
+                    function splitArrays (array){
+                        for (let i=0; i<array.length; i++){
+                            let tempArray = array[i].split(" ")
+                            tempArray = tempArray.filter(function (el) {
+                                return el != '';
+                            })
+                            result.push({
+                                ipAddress:tempArray[0],
+                                macAddress:tempArray[1],
+                                expires:tempArray[2],
+                                type:tempArray[3],
+                                interface:tempArray[4]
+                            })
+                        }
                     }
+                    splitArrays(resu)
+                    res.send(result)
 
-                    console.log(newArray)
-                    console.log("############################")
+                })
 
-                    // for (let j=0; j<newArray.length; j++){
-                    //     console.log(newArray[j])
-                    //     // if (newArray[j]==''){
-                    //         console.log("entering here")
-                    //         // newArray.splice(j, 1);
-                    // //         let filteredArray1 = newArray[j].filter(function (el) {
-                    // //         return el != null;
-                    // //   });
-                    // //   filteredArray.push(filteredArray1)
-                    //     // }
-                    // }
-                }
-            }
-            splitArrays(resu)
+                .on('data', data => {
+                    datatobSent = datatobSent + data;
+                })
+                .stderr.on('data', data => {
+                    console.log('STDERR: ' + data);
+                });
 
-            
-            let result = [{
-                //ipAddresxs:datatobSent.substring(index, index+11),
-                macAddress:[],
-                expires:[],
-                interface:[],
-            }]
-            //JSON.parse(datatobSent)
-            // console.log(datatobSent)
-             res.send(datatobSent)
+                stream.end('show router arp\nexit\n');
+            });
 
         })
-        .on('data', function(data) {
-            // console.log(""+data)
-            datatobSent = datatobSent + data;
-
-        }).stderr.on('data', function(data) {
-            console.log('STDERR: ' + data);
+        .connect({
+            host: '10.0.0.240',
+            port: 22,
+            username: process.env.ROUTER_USERNAME,
+            password: process.env.ROUTER_PASSWORD,
+            algorithms: {
+                kex:['diffie-hellman-group1-sha1'],
+                },
+                // debug: function (output){
+                //     console.log(output)
+                // }
         });
-        stream.end('show router arp\nexit\n');
-    });
-}).connect({
-  host: '10.0.0.240',
-  port: 22,
-  username: 'admin',
-  password:'admin',
-  algorithms: {
-    kex:['diffie-hellman-group1-sha1'],
-    },
-    // debug: function (output){
-    //     console.log(output)
-    // }
-});
     }
 
 }
