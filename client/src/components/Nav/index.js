c2e2f81b0306d6dfb359c6c57fe47ea433b8090e
M L <mlecontenyc@gmail.com>
2019-03-21 18:43:05 -0400

    formated main page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,7 +7,7 @@ function Nav(props) {
     <nav>
         <div className="nav-wrapper #0091ea light-blue accent-4">
         <img src={logo} className="fit-picture"></img>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <ul id="nav-mobile" className="right hide-on-med-and-down link">
             <li><a href="/mirror">Mirrors</a></li>
             <li><a href="/">Search</a></li>
         </ul>
